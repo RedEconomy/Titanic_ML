a3cf9da72c2c2627baccbfe71074099efe927cd4
Jack <147711299+RedEconomy@users.noreply.github.com>
2025-02-09 13:14:20 +0100

    Update DecisionTreeModel.py

--- src/DecisionTreeModel.py
+++ src/DecisionTreeModel.py
@@ -1,5 +1,5 @@
 from sklearn.tree import DecisionTreeClassifier
-model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=4,
-                               min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=23,
+model = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=5,
+                               min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=51,
                                max_leaf_nodes=None, min_impurity_decrease=0.0, class_weight=None, ccp_alpha=0.0, monotonic_cst=None)
 model.fit(X_train, Y_train)
